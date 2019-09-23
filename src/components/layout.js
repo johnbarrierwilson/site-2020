c66c4f658f577ca1e40f98bd1bb419250fc85a09
John Barrier Wilson <johnbarrierwilson@gmail.com>
2019-09-22 21:20:19 -0500

    Create hompage hero section

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,14 +23,16 @@ class Layout extends React.Component {
       )
     }
     return (
-      <div>
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <div className="row">
+        <div className="cell">
+          <header>{header}</header>
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
       </div>
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,40 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
+import Image from "gatsby-image"
+import Layout from '../components/layout'
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <article key={node.fields.slug}>
-              <header>
-                <h3>
-                  <Link to={node.fields.slug}>
-                    {title}
-                  </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-              </header>
-              <section>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-              </section>
-            </article>
-          )
-        })}
-      </Layout>
-    )
-  }
+const IndexPage = (props) => {
+  const { data } = props
+  const siteTitle = data.site.siteMetadata.title
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <div className="cell cell--s well well--l tac">
+        <Image
+          alt="portrait of John Barrier Wilson"
+          className="avatar"
+          fixed={data.avatar.childImageSharp.fixed}
+        />
+        <h1>Product Designer at&nbsp;<a href="https://www.elastic.co">Elastic</a></h1>
+        <p>Focused on code, users, interactions and visuals. Creating and sharing content for other senior designers who love what they do.</p>
+      </div>
+    </Layout>
+  )
 }
 
-export default BlogIndex
+export default IndexPage
 
 export const pageQuery = graphql`
   query {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 82, height: 82) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
     site {
       siteMetadata {
         title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
       }
     }
   }
